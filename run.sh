e747f819d7d3e07bd8ea7d16279fa7705e2abf68
*TlSunGQf66 <kazuki@hacarus.com>
2021-01-29 19:36:20 +0900

    feat: setup message when app is running on background

--- run.sh
+++ run.sh
@@ -5,8 +5,13 @@ fi
 if [ "$1" = "-d" -o "$2" = "-d" ]; then
     # background
     docker-compose up -d
+
+    port=$(cat app/utils/config.ini | sed -n 2p | tr -d "port = ")
     echo
     echo "[SUCCESS] app running as docker-compose on background!"
+    echo "------------------------------------------------------"
+    echo "⇨ http server started on [::]:${port}"
+    echo
     echo "(To terminate app: \"docker-compose down\")"
     echo
 else
